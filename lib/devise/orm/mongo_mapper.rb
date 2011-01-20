c432f4a95b352db2bb086bc5852719b8e945ec44
bhbryant <benjamin@bryantmarkowsky.com>
2011-01-20 09:28:50 -0800

    added options to orm

--- lib/devise/orm/mongo_mapper.rb
+++ lib/devise/orm/mongo_mapper.rb
@@ -14,13 +14,27 @@ module Devise
           include Validatable
           yield
           return unless Devise.apply_schema
-          devise_modules.each { |m| send(m) if respond_to?(m, true) }
-        end                
+
+          devise_modules.each do |m|
+            if respond_to?(m, true) 
+              if defined?(@@orm_options) && @@orm_options.has_key?(m)
+                 send(m, @@orm_options[m] ) 
+               else
+                 send(m)
+              end
+            end
+          end
+        end   
+        
+        def orm_options=(options)
+          @@orm_options = options
+        end
+   
+                     
       end
     end
   end
 end
-
 MongoMapper::Document.append_extensions(Devise::Models)
 MongoMapper::Document.append_extensions(Devise::Orm::MongoMapper::Hook)
 
